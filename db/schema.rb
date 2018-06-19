e94f41cda99d15b91e29d91fc73dd1394c6d26fe
Kostlba <mrjyraa@gmail.com>
2018-06-19 21:31:16 +0300

    add avatar for dish

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180530205708) do
+ActiveRecord::Schema.define(version: 20180619174942) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -58,6 +58,10 @@ ActiveRecord::Schema.define(version: 20180530205708) do
     t.integer "user_id"
     t.integer "price"
     t.integer "category_id"
+    t.string "logo_file_name"
+    t.string "logo_content_type"
+    t.integer "logo_file_size"
+    t.datetime "logo_updated_at"
   end
 
   create_table "userparams", force: :cascade do |t|
@@ -84,6 +88,7 @@ ActiveRecord::Schema.define(version: 20180530205708) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
